d522ca481f11111996feb7a8236f460ac2965120
Shantanu Khopade <shantanu.s.khopade@gmail.com>
2025-08-11 19:31:04 +0530

    committed

--- src/main/java/com/shantanu/product_inventory/models/Admin.java
+++ src/main/java/com/shantanu/product_inventory/models/Admin.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(name = "Admin")
 @Getter
--- src/main/java/com/shantanu/product_inventory/services/ExcelService.java
+++ src/main/java/com/shantanu/product_inventory/services/ExcelService.java
@@ -3,7 +3,6 @@ package com.shantanu.product_inventory.services;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
-import java.util.Objects;
 
 public interface ExcelService {
     // is the file an Excel file or not
